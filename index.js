f9e83a9c2455d2e3846cb7507d436f3fa9b2f45f
nathalia <nathaliatroina@gmail.com>
2022-01-17 18:10:40 -0500

    commit tree 533302ecac5fc8f1d14f3ea769dc887cf9b8a0ac

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2)
+    //if i put minus it is gonna start from the back
+    // and it goes until the end 
+}
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+ //I can put a varible inside of an array
+
+function createFareMultiplier(price){
+    return function (multiplier){
+        return multiplier * price
+    }
+}
+ 
+ const fareDoubler = function (fare){
+     return createFareMultiplier(fare)(2)
+ }
+
+ const fareTripler = function(fare){
+    return createFareMultiplier(fare)(3)
+ }
+
+ function selectDifferentDrivers(drivers, selectFunction){
+     return selectFunction(drivers)
+ }
